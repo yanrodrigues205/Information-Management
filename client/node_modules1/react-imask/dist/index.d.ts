48fccfd3ad6bea8d7bfd7c6b9ca2735acc0b091b
Yan._.Code <92941649+yanrodrigues205@users.noreply.github.com>
2023-03-08 23:26:58 -0300

    delete * from node_modules1

--- client/node_modules1/react-imask/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import 'imask';
-export { default as IMask } from 'imask';
-export { default as IMaskMixin } from './mixin';
-export { default as IMaskInput } from './input';
-export { default as useIMask } from './hook';
